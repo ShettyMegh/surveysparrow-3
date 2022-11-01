84aa839635923020591247ce27bc1de9d0fb29d1
meghan87 <115980448+meghan87@users.noreply.github.com>
2022-11-01 11:12:43 +0530

    steps and collapse
    made major design change for steps in mobile version and added collapse functionality using javascript

--- js/script.js
+++ js/script.js
@@ -6,6 +6,17 @@ const getElement = (sele)=>{
     )
 }
 
+const getElements = (sele)=>{
+    let ele = document.querySelectorAll(sele);
+    if(ele) return ele;
+    throw Error(
+        `${sele} not found`
+    )
+}
+
+
+
+// nav toggle button
 const toggleBtn = getElement('#toggleBtn');
 
 toggleBtn.addEventListener('click',function(){
@@ -16,4 +27,25 @@ toggleBtn.addEventListener('click',function(){
 
 function addClass(ele){
     ele.classList.toggle('show-links');
+}
+
+
+
+// collapse bar
+const collapseItems = getElements('.collapse-item');
+console.log(collapseItems);
+collapseItems.forEach((collapseItem)=>{
+    collapseItem.addEventListener('click',function(){
+        removeClass(collapseItems,'collapse-active')
+        let className = this.className;
+        let headerLogo = getElement(`.${className} .collapse-header p`)
+        // headerLogo.style.transform = "rotate(30deg)";
+        this.classList.toggle('collapse-active');
+    })
+})
+
+function removeClass(elements,className){
+    elements.forEach((element)=>{
+        element.classList.remove(className);
+    })
 }
